7ad2667d22658a09be2f664ea8a067648f29d0d7
George <tro37295@gmail.com>
2022-07-07 23:57:33 +0500

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов случайными числами и выводит их на экран. 
+// Оформите заполнение массива и вывод в виде функции (пригодится в следующих задачах)
+// 1,2,5,7,19,1,2,8 -> 1,2,5,7,19,1,2,8 
+// 6,1,33,5,6,7,7,1 -> 6,1,33,5,6,7,7,1
+
+int RandomNum(int min, int max)
+{
+    int a = new Random().Next(min, max + 1);
+    return a;
+}
+
+int[] NewArray(int length, int min, int max)
+{
+    int[] arr = new int[length];
+    int i = 0;
+    while (i < length)
+    {
+    
+        arr[i] = RandomNum(min, max);
+        i++;
+    }
+    return arr;
+}
+
+void Print(int[] array)
+{
+    System.Console.WriteLine("[");
+    System.Console.WriteLine(array[0]);
+    for (int i = 1; i < array.Length; i++)
+    {
+        System.Console.WriteLine(", " + array[i]);
+    }
+}
+
+int[] array1 = NewArray(8, 0, 99);
+Print(array1);
